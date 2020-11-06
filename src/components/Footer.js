e1cfd9a23a0396e3224c779f51ec483d2e2572bf
codemish <73407426+codemish@users.noreply.github.com>
2020-11-06 15:32:10 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,8 @@ const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
         
         <p className="copyright">
-         Built with <a href="http://gatsby-dimension.surge.sh">Gatsby.js</a> and ❤️ by <a href="http://www.adityaketkar.me"> Aditya Ketkar</a>  
+         Built with ❤️ by <a href="http://www.adityaketkar.me">Nimish Khandelwal</a>  
         <br/>
-        Get your own version of this webpage <a href="https://github.com/adityaketkar/circle-packing-personal-homepage"> here </a>
         </p>
     </footer>
 )
